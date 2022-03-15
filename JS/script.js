88629551d464028a6bccb4129dfcfe5a6601c282
Ann Wanjeri <wannjeri2@gmail.com>
2022-03-15 10:46:36 +0300

    st-services card jquerry toggle effect

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,26 @@
 //what we do section
-$().ready(function () {
-  console.log("page loaded.");
-  $(".card1").click(() => {
-    $(".card-heading1").toggle("slow");
-    $().ready(function () {
-    $(".card3-text").toggle("slow");
-  }); 
 
-//   form validation
-  $("button#button").click(function (event) {
-    let name = $("#inputname").val();
-    let email = $("#inputEmail").val();
-    let message = $("#mymessage").val();
-
-    if ($("#inputname").val() && $("#inputEmail").val()) {
-      alert(
-        "Hi " +
-          name +
-          ",Thank you for reaching out to us.One of us will contact you shortly."
-      );
-    } else {
-      alert("Please fill the form");
-    }
+$(document).ready(function () {
+  $(".mt-3").click(function () {
+    $(".hide", this).toggle(1000);
+    $(".show", this).toggle();
   });
 });
+
+// //   form validation
+//   $("button#button").click(function (event) {
+//     let name = $("#inputname").val();
+//     let email = $("#inputEmail").val();
+//     let message = $("#mymessage").val();
+
+//     if ($("#inputname").val() && $("#inputEmail").val()) {
+//       alert(
+//         "Hi " +
+//           name +
+//           ",Thank you for reaching out to us.One of us will contact you shortly."
+//       );
+//     } else {
+//       alert("Please fill the form");
+//     }
+//   });
+// });
